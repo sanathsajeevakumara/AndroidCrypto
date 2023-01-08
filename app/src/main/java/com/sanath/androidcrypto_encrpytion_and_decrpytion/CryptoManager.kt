c6d97ef3a7e971da60c134577cbdee7e86c10195
Sanath <sanath@entrusttitle.com>
2023-01-08 09:43:18 +0530

    Decryption Code is added and Encrypt Decrypt functions are created

--- app/src/main/java/com/sanath/androidcrypto_encrpytion_and_decrpytion/CryptoManager.kt
+++ app/src/main/java/com/sanath/androidcrypto_encrpytion_and_decrpytion/CryptoManager.kt
@@ -4,10 +4,14 @@ import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import androidx.annotation.RequiresApi
+import java.io.InputStream
+import java.io.OutputStream
 import java.security.KeyStore
 import javax.crypto.Cipher
+import javax.crypto.CipherOutputStream
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 @RequiresApi(Build.VERSION_CODES.P)
 class CryptoManager {
@@ -17,7 +21,20 @@ class CryptoManager {
     }
 
     private val encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
-        init(Cipher.ENCRYPT_MODE, createKey())
+        init(
+            Cipher.ENCRYPT_MODE,
+            getKey(),
+        )
+    }
+
+    private fun decryptCipherForIv(iv: ByteArray): Cipher {
+        return Cipher.getInstance(TRANSFORMATION).apply {
+            init(
+                Cipher.DECRYPT_MODE,
+                getKey(),
+                IvParameterSpec(iv),
+            )
+        }
     }
 
     private fun getKey(): SecretKey {
@@ -39,6 +56,31 @@ class CryptoManager {
                     .build()
             )
         }.generateKey()
+    }
+
+    fun encrypt(bytes: ByteArray, outputStream: OutputStream): ByteArray {
+        val encryptedByte = encryptCipher.doFinal(bytes)
+        outputStream.use {
+            it.write(encryptCipher.iv.size)
+            it.write(encryptCipher.iv)
+            it.write(encryptedByte.size)
+            it.write(encryptedByte)
+        }
+        return encryptedByte
+    }
+
+    fun decrypt(inputStream: InputStream): ByteArray {
+        return inputStream.use {
+            val ivSize = it.read()
+            val iv = ByteArray(ivSize)
+            it.read(iv)
+
+            val encryptedByteSize = it.read()
+            val encryptedByte = ByteArray(encryptedByteSize)
+            it.read(encryptedByte)
+
+            decryptCipherForIv(iv).doFinal(encryptedByte)
+        }
     }
 
     companion object {
